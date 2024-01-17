3d9edd0ef02bda9d5dcd6208e41cb3ab0b8ae9d5
오동현 <dhgus44@naver.com>
2024-01-18 02:51:49 +0900

    commit test

---
+++ src/main/java/com/example/realpraise/RealPraiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.realpraise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealPraiseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RealPraiseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/realpraise/RealPraiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realpraise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealPraiseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
