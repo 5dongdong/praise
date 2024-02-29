428a9664eff12e95ae7a749d208ac138012a08c1
오동현 <dhgus44@naver.com>
2024-02-29 23:03:38 +0900

    feat: UserResponse엔티티 추가

---
+++ src/main/java/com/example/realpraise/entity/UserResponses.java
@@ -0,0 +1,28 @@
+package com.example.realpraise.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class UserResponses {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long responseId;
+
+  @ManyToOne
+  @JoinColumn(name = "userId")
+  private User user;
+
+  @ManyToOne
+  @JoinColumn(name = "quizId")
+  private Quiz quiz;
+
+  @ManyToOne
+  @JoinColumn(name = "optionId")
+  private Options options;
+}
