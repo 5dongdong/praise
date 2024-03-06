30adddd1881dc822b2ecaeb4e44de49e6d73ed6f
오동현 <dhgus44@naver.com>
2024-03-06 23:57:16 +0900

    feat : 유저정보 전달

--- src/main/java/com/example/realpraise/controller/QuizController.java
+++ src/main/java/com/example/realpraise/controller/QuizController.java
@@ -1,31 +1,36 @@
 package com.example.realpraise.controller;
 
 import com.example.realpraise.entity.Quiz;
+import com.example.realpraise.entity.QuizUser;
+import com.example.realpraise.repository.QuizRepository;
+import com.example.realpraise.repository.QuizUserRepository;
 import com.example.realpraise.service.QuizService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @AllArgsConstructor
 public class QuizController {
 
   private QuizService quizService;
+  private QuizUserRepository quizUserRepository;
 
-//  @GetMapping("/quiz/{questionId}")
   @GetMapping("/quiz/{questionId}")
   public String quiz(@PathVariable Long questionId, Model model) {
     Quiz quiz = quizService.getQuizById(questionId);
     model.addAttribute("Quiz", quiz);
-    return "quiz";
+    return "/quiz";
   }
 
-  @RequestMapping("/praise")
+  @RequestMapping("/index")
   public String praise() {
-    return "/praise";
+    return "/index";
   }
 
   @RequestMapping("/test")
@@ -33,9 +38,23 @@ public class QuizController {
     return "/test";
   }
 
-  @RequestMapping("/donghyeon")
-  public String donghyeon() {
-    return "/donghyeon";
+  @RequestMapping("/login")
+  public String login() {
+    return "/login";
+  }
+
+  @PostMapping("/saveUserName")
+  public String saveUserName(QuizUser user, RedirectAttributes redirectAttributes) {
+    QuizUser savedUser = quizUserRepository.save(user);
+    redirectAttributes.addAttribute("userId", savedUser.getUserId());
+    return "redirect:/success-page/{userId}";
+  }
+
+  @GetMapping("/success-page/{userId}")
+  public String showUserTest(@PathVariable Long userId, Model model) {
+    QuizUser user = quizUserRepository.findById(userId).orElseThrow();
+    model.addAttribute("user", user);
+    return "success-page";
   }
 
 }
