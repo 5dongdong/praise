dca429039684f2786d757e024925f02fda614e85
오동현 <dhgus44@naver.com>
2024-03-09 22:21:56 +0900

    feat : QuizController 필요없는 코드 삭제

--- src/main/java/com/example/realpraise/controller/QuizController.java
+++ src/main/java/com/example/realpraise/controller/QuizController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
@@ -48,6 +51,7 @@ public class QuizController {
     model.addAttribute("user", user);
     return "quiz";
   }
+
 
 
 }
