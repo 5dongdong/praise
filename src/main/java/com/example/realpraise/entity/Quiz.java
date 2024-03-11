84c6b78f3ef6590afc4cb10f4d2a7a51412f9000
오동현 <dhgus44@naver.com>
2024-03-11 21:03:50 +0900

    feat : Quiz엔티티 변경

--- src/main/java/com/example/realpraise/entity/Quiz.java
+++ src/main/java/com/example/realpraise/entity/Quiz.java
@@ -4,15 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Quiz {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long quizId;
-  private String question;
 
+  @Lob
+  private String question;
   private int score;
   private int answer;
 }
