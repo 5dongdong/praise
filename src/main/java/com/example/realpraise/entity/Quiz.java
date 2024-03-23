cd865b5e62032a3295b6db4d4c292a4beb92ca8f
오동현 <dhgus44@naver.com>
2024-03-23 22:28:07 +0900

    feat : Quiz맵핑 수정

--- src/main/java/com/example/realpraise/entity/Quiz.java
+++ src/main/java/com/example/realpraise/entity/Quiz.java
@@ -1,22 +1,32 @@
 package com.example.realpraise.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
+
+import java.util.List;
 
 @Entity
 @Getter
 public class Quiz {
 
   @Id
+  @Column(name = "quizId")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long quizId;
 
   @Lob
   private String question;
+
   private int score;
+
   private int answer;
+
+  @OneToMany(mappedBy = "quiz")
+  private List<Options> options;
 }
